9e54b7bf75bad2aa26073a46d06a50777dc2594a
RizkyriaHutabarat <rizkyriahutabarat18@gmail.com>
2023-04-07 18:02:34 +0700

    fe_Rizkyria

--- js/table.js
+++ js/table.js
@@ -12,6 +12,9 @@ export let isiTabel = `
     </th>
     <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-800 text-left">#USERNAME#</th>
     <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-800 text-center">#SEKOLAH#</th>
+    <div class="w-auto p-2">
+        <div class="flex items-center justify-center w-10 h-10 text-base font-medium text-#WARNALOGO#-600 bg-#WARNALOGO#-200 rounded-md">ULBI</div>
+    </div>
     <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-500 text-left">#JABATAN#</th>
     <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-#col#-500 text-left">#LULUSAN#</th>
     <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-#col#-500 text-left">#SKILL#</th>
